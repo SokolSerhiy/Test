2d1a1cde7834eed53e1a86726548d6ef38cd4660
Sokol Sergiy <sokol.serhij@gmail.com>
2017-09-20 20:08:35 +0300

    Signed-off-by: Sokol Sergiy <sokol.serhij@gmail.com>

--- Demo-for-heroku/src/main/java/ua/lviv/lgs/DemoForHerokuApplication.java
+++ Demo-for-heroku/src/main/java/ua/lviv/lgs/DemoForHerokuApplication.java
@@ -1,11 +1,15 @@
 package ua.lviv.lgs;
 
+import java.io.IOException;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 
 @SpringBootApplication
 public class DemoForHerokuApplication {
@@ -18,9 +22,13 @@ public class DemoForHerokuApplication {
 	String cloudinaryUrl;
 	
 	
+	@SuppressWarnings("unchecked")
 	@Bean
-	public Cloudinary cloudinary() {
+	public Cloudinary cloudinary() throws IOException {
 		System.out.println("Hello from cloudinary----------------------------------------");
-		return new Cloudinary(cloudinaryUrl);
+		Cloudinary cloudinary = new Cloudinary(cloudinaryUrl);
+		Map<String, String> uploadResult = cloudinary.uploader().upload("https://cloudinary-res.cloudinary.com/image/asset/dpr_2.0/logo-e0df892053afd966cc0bfe047ba93ca4.png", ObjectUtils.emptyMap());
+		System.out.println(uploadResult.get("url"));
+		return cloudinary;
 	}
 }
