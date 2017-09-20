ed2542a040ed354cb6110a585ebb8e1c75da6685
Sokol Sergiy <sokol.serhij@gmail.com>
2017-09-20 19:59:30 +0300

    Signed-off-by: Sokol Sergiy <sokol.serhij@gmail.com>

--- Demo-for-heroku/src/main/java/ua/lviv/lgs/DemoForHerokuApplication.java
+++ Demo-for-heroku/src/main/java/ua/lviv/lgs/DemoForHerokuApplication.java
@@ -1,12 +1,25 @@
 package ua.lviv.lgs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.cloudinary.Cloudinary;
 
 @SpringBootApplication
 public class DemoForHerokuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoForHerokuApplication.class, args);
+	}
+	
+	@Value("${cloudinary.url}")
+	String cloudinaryUrl;
+	
+	
+	@Bean
+	public Cloudinary cloudinary() {
+		return new Cloudinary(cloudinaryUrl);
 	}
 }
