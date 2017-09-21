b58b76445bafec7f9920d604318ef1985ebc187d
Sokol Sergiy <sokol.serhij@gmail.com>
2017-09-21 18:12:19 +0300

    Signed-off-by: Sokol Sergiy <sokol.serhij@gmail.com>

--- Demo-for-heroku/src/main/java/ua/lviv/lgs/DemoForHerokuApplication.java
+++ Demo-for-heroku/src/main/java/ua/lviv/lgs/DemoForHerokuApplication.java
@@ -22,13 +22,13 @@ public class DemoForHerokuApplication {
 	String cloudinaryUrl;
 	
 	
-	@SuppressWarnings("unchecked")
-	@Bean
-	public Cloudinary cloudinary() throws IOException {
-		System.out.println("Hello from cloudinary----------------------------------------");
-		Cloudinary cloudinary = new Cloudinary(cloudinaryUrl);
-		Map<String, String> uploadResult = cloudinary.uploader().upload("https://cloudinary-res.cloudinary.com/image/asset/dpr_2.0/logo-e0df892053afd966cc0bfe047ba93ca4.png", ObjectUtils.emptyMap());
-		System.out.println(uploadResult.get("url"));
-		return cloudinary;
-	}
+//	@SuppressWarnings("unchecked")
+//	@Bean
+//	public Cloudinary cloudinary() throws IOException {
+//		System.out.println("Hello from cloudinary----------------------------------------");
+//		Cloudinary cloudinary = new Cloudinary(cloudinaryUrl);
+//		Map<String, String> uploadResult = cloudinary.uploader().upload("https://cloudinary-res.cloudinary.com/image/asset/dpr_2.0/logo-e0df892053afd966cc0bfe047ba93ca4.png", ObjectUtils.emptyMap());
+//		System.out.println(uploadResult.get("url"));
+//		return cloudinary;
+//	}
 }
